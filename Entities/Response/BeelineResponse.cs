2bf7e9b497d5213465e94a5863b939c7a8a55eee
r.medvedev <r.e.medvedev96@gmail.com>
2025-05-20 17:25:36 +0600

    add beeline provider

---
+++ Entities/Response/BeelineResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Kp.Ms.Sms.Entities.Response;
+
+public class BeelineResponse
+{
+    [JsonProperty("actions")]
+    public List<BeelineAction>? Actions { get; set; }
+
+    [JsonProperty("agt_id")]
+    public string? AgentId { get; set; }
+
+    [JsonProperty("error")]
+    public BeelineError? Error { get; set; }
+
+    [JsonProperty("date_report")]
+    public string ReportDate { get; set; }
+}
--- Providers/ProviderNames.cs
+++ Providers/ProviderNames.cs
@@ -4,5 +4,6 @@ public enum ProviderNames
 {
     SmsRu,
     Megafon,
-    SMSC
+    SMSC,
+    Beeline
 }
